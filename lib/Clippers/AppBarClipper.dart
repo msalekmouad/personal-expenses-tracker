4a59eebc5da2a5ddd820f454ae4f4f1ad3a52759
msalekmouad <mouadmsalek@outlook.com>
2020-08-26 16:34:03 +0100

    Added custom app bar and listing transactions

---
+++ lib/Clippers/AppBarClipper.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppBarClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    // TODO: implement getClip
+    var path = new Path();
+    path.lineTo(0, size.height - 40);
+    var controllPoint = Offset(50, size.height);
+    var endPoint = Offset(size.width/2, size.height);
+    path.quadraticBezierTo(controllPoint.dx, controllPoint.dy, endPoint.dx, endPoint.dy);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return true;
+  }
+
+}
